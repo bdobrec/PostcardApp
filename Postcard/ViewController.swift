3469be9f98f5edfce5cb8156a632f934d124b116
Brandon Dobrec <bdobrec@Brandons-MacBook-Air.local>
2015-04-27 15:46:15 -0700

    Initial Commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Brandon Dobrec on 4/27/15.
+//  Copyright (c) 2015 Brandon Dobrec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
